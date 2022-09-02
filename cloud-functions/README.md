820df57ae02aa4eaf5480ea1aed3ca034df06718
Trung Doan <asd135hp1@gmail.com>
2022-09-03 06:54:02 +1000

    Quick fix

--- cloud-functions/README.md
+++
@@ -1,2 +0,0 @@
-# WARNING
-Code written in this folder is used for deploying to Cloud Functions after compilation. Code in this folder can not interact with other code in other files
--- cloud-functions/decompressSnapshot.js
+++
@@ -1,9 +0,0 @@
-const { Firestore } = require("@google-cloud/firestore")
-const { Storage }= require("@google-cloud/storage")
-
-const firestore = new Firestore({ projectId: process.env.GOOGLE_CLOUD_PROJECT })
-const storage = new Storage({ projectId: process.env.GOOGLE_CLOUD_PROJECT })
-
-exports.decompressSnapshot = event => {
-  console.log(event)
-}

09ca971c5a989ec02fc4034c28a16a83cc82fde4
Trung Doan <asd135hp1@gmail.com>
2022-09-16 00:50:04 +1000

    Committed using npm run

--- src/model/json.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JsonConvertible = void 0;
-class JsonConvertible {
-    toJson() { return {}; }
-    static fromJson(json) { return new JsonConvertible(); }
-}
-exports.JsonConvertible = JsonConvertible;
-//# sourceMappingURL=json.js.map
